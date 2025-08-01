b13ee401d0d37098e9a61d91bda5973029a4d894
vipinshivhare <vipinshivhare804@gmail.com>
2025-08-01 12:32:35 +0530

    Initial commit for corporate-travel-api

---
+++ README.md
@@ -0,0 +1,1 @@
+# corporate-travel-api
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+const data = require("./data.json");
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ API is running at http://localhost:${PORT}/data`);
+});
